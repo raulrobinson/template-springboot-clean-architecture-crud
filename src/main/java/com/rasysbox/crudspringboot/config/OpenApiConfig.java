84b78e5cf52374f4c8fce0d0d740def5c88eb10c
rasys <rasysbox@hotmail.com>
2024-11-04 22:04:12 -0500

    first commit

--- src/main/java/com/rasysbox/crudspringboot/config/OpenApiConfig.java
+++ src/main/java/com/rasysbox/crudspringboot/config/OpenApiConfig.java
@@ -1,32 +1,73 @@
 package com.rasysbox.crudspringboot.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "API of Users",
-                version = "1.0.0",
-                description = "API for managing users",
-                contact = @Contact(
-                        name = "Raul Bolivar Navas",
-                        url = "https://rasysbox.com",
-                        email = "rasysbox@hotmail.com"
-                ),
-                license = @License(
-                        name = "MIT License",
-                        url = "https://opensource.org/licenses/MIT"
-                )
-        ),
-        servers = {
-                @Server(url = "http://localhost:8080", description = "Local Server"),
-                @Server(url = "https://rasysbox.com", description = "Production Server")
-        }
-)
-public class OpenApiConfig {}
+public class OpenApiConfig {
+
+    @Value("${api.title}")
+    private String title;
+
+    @Value("${api.version}")
+    private String version;
+
+    @Value("${api.description}")
+    private String description;
+
+    @Value("${api.contact.name}")
+    private String contactName;
+
+    @Value("${api.contact.url}")
+    private String contactUrl;
+
+    @Value("${api.contact.email}")
+    private String contactEmail;
+
+    @Value("${api.license.name}")
+    private String licenseName;
+
+    @Value("${api.license.url}")
+    private String licenseUrl;
+
+    @Value("${api.server.local.url}")
+    private String localServerUrl;
+
+    @Value("${api.server.local.description}")
+    private String localServerDescription;
+
+    @Value("${api.server.production.url}")
+    private String productionServerUrl;
+
+    @Value("${api.server.production.description}")
+    private String productionServerDescription;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .version(version)
+                        .description(description)
+                        .contact(new Contact()
+                                .name(contactName)
+                                .url(contactUrl)
+                                .email(contactEmail))
+                        .license(new License()
+                                .name(licenseName)
+                                .url(licenseUrl)))
+                .servers(List.of(
+                        new Server().url(localServerUrl).description(localServerDescription),
+                        new Server().url(productionServerUrl).description(productionServerDescription)
+                ));
+    }
+}
